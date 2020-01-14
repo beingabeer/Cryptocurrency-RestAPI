c6e0289432c6cf21144a0c60ee9298ba650b0954
abeer <abeer.sewak@gmail.com>
2020-01-13 18:27:10 -0600

    added view for api

--- trackingcryptocurrency/trackingAPI/views.py
+++ trackingcryptocurrency/trackingAPI/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Cryptocurrency
+from .serializers import CryptocurrencySerializer
 
-# Create your views here.
+
+class ListCryptocurrencyView(generics.ListAPIView):
+    queryset = Cryptocurrency.objects.all()
+    serializer_class = CryptocurrencySerializer
